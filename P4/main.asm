a792b5cd08dfb03a48097ae8012f303af8709d3d
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-10-26 11:30:01 -0600

    feat: practice 4 part 2 code

--- P4/main.asm
+++ P4/main.asm
@@ -17,18 +17,83 @@
           out ddrd, mode; configura ddrc como salida
 
 
-fin:      ldi led, $FF; carga un valor de 1 a led
+fin:      ldi led, $01; carga un valor de 1 a led
           out portd, led ; manda a d el contenido de led
           rcall retardo ; retardo
           ldi led, $00; carga un valor de 0 a led
           out portd, led ; manda a d el contenido de led
           rcall retardo ; retardo
+
+          ldi led, $02; carga un valor de 2 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+          ldi led, $00; carga un valor de 0 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+
+          ldi led, $04; carga un valor de 4 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+          ldi led, $00; carga un valor de 0 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+
+          ldi led, $08; carga un valor de 8 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+          ldi led, $00; carga un valor de 0 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+
+          ldi led, $0F; carga un valor de 15 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+          ldi led, $00; carga un valor de 0 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+
+          ldi led, $10; carga un valor de 16 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+          ldi led, $00; carga un valor de 0 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+
+          ldi led, $20; carga un valor de 32 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+          ldi led, $00; carga un valor de 0 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+
+          ldi led, $40; carga un valor de 64 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+          ldi led, $00; carga un valor de 0 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+
+          ldi led, $80; carga un valor de 128 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+          ldi led, $00; carga un valor de 0 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+
+          ldi led, $F0; carga un valor de 240 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+          ldi led, $00; carga un valor de 0 a led
+          out portd, led ; manda a d el contenido de led
+          rcall retardo ; retardo
+
+
           rjmp fin
 
 
-retardo:  ldi c, $F6 ;1 CM
-loop3:    ldi a, $F6 ; 1 CM
-loop2:    ldi b, $F6 ;1 CM
+retardo:  ldi c, $30 ;1 CM
+loop3:    ldi a, $30 ; 1 CM
+loop2:    ldi b, $30 ;1 CM
           ;el bloque loop tarda 1000 CM por cada vez que se llame
 loop:     dec b; 1 (255)
           cpi b, $00 ;1 (255)
